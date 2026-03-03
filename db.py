8b08a4324cce2ca21eb497db763777a5cf3bda5b
LyzaIamrache <li2mn@umkc.edu>
2026-03-03 06:56:18 -0100

    Add initial imports for database connection

---
+++ db.py
@@ -0,0 +1,5 @@
+import os
+from dotenv import load_dotenv
+import snowflake.connector
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
